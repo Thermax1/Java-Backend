2a4454f969229b7068a5cc2ad00d319f7bb147ee
Thermax1 <a.d.munoz23@gmail.com>
2023-12-19 17:29:02 +0100

    Created Java project for language app

--- get-going-with-gradle/build.gradle.kts
+++ get-going-with-gradle/build.gradle.kts
@@ -7,3 +7,15 @@ tasks.named<Jar>("jar") {
         attributes["Main-Class"] = "com.gradlehero.languageapp.SayHello"
     }
 }
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
